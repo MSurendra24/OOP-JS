bdd50513d40e0fbb83aa973afe70d0c7d67263ed
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-25 10:28:27 +0530

    one

---
+++ ES6.JS/example.js
@@ -0,0 +1,12 @@
+let nums=[12,44,34,55,67];
+let num=nums.filter((nu)=>{
+    return nu%2==0
+})
+console.log(num);
+
+
+let ar=[12,33,45,56,78,97];
+let br=ar.filter(a=>a%2!==0)
+console.log(br);
+
+
--- ES6.JS/two.js
+++ ES6.JS/two.js
@@ -1,5 +1,11 @@
-let id=[1,2,3,4,5]
-for(let id of eid){
-    console.log(id)
+// let id=[1,2,3,4,5]
+// for(let id of eid){
+//     console.log(id)
 
-}
+// }
+setTimeout(()=>{
+    console.log("Good Morning")
+},2000)
+setInterval(()=>{
+    console.log("Good Morning")
+},5000)
