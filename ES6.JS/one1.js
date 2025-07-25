21aa28fcb42dc0597250e2ee32b0f2c6b7355260
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-25 07:24:18 +0530

    one

---
+++ ES6.JS/one1.js
@@ -0,0 +1,9 @@
+let employeeid=[12,98,34,76];
+let employeenum=employeeid.forEach((id)=>(id+1))
+console.log(employeeid)
+console.log(employeenum)
+
+let employeesal=[1234,56789];
+let employeeamount=employeesal.map((sal=>(sal+1)))
+console.log(employeesal)
+console.log(employeeamount)
---
+++ ES6.JS/practise.js
@@ -0,0 +1,18 @@
+// let employee=[12,34,56,65,43,21];
+// let employees=[]
+// employee.forEach((employees)=>{
+//     console.log(employees +1)
+
+// })
+// console.log(employees)
+// console.log(employee)
+
+let eid=[98,76,54,32];
+let sid=eid.forEach((id)=>{
+    return id+1;
+})
+
+
+
+console.log(eid)
+console.log(sid)
